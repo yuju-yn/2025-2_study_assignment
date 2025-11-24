065ae42bad7913b411bed0026e2a72b8795e23f0
yuju-yn <juyeon.yu1008@gmail.com>
2025-11-24 14:56:19 +0900

    assignment4 과제 제출

---
+++ assignment4/Run Sample 1/Assets/TestFolder/Car.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car
+{
+    int speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Car started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
